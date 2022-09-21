331a928f68a529b17d9cac69c345d8df8d4a5ce9
Fabricio Martinho Engeroff <fa.engeroff1996@gmail.com>
2022-09-21 09:00:56 -0300

    H2 database

---
+++ beckend/src/main/java/com/dvFabricio/dscatalog/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.dvFabricio.dscatalog.services;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dvFabricio.dscatalog.entities.Category;
+import com.dvFabricio.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+		
+		
+}
